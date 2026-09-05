90f73e2013cba3886cfb81b9a8a648973bfdac4a
pierrecahuzac <cahuzac.p@gmail.com>
2026-09-05 13:14:09 +0200

    fix(responsive): joueurs inscrits sous footer
    - web/src/App.jsx: pb-32 -> pb-36
    - web/src/pages/Accueil.jsx: ajoute spacer h-6 après liste joueurs inscrits

--- web/src/pages/Accueil.jsx
+++ web/src/pages/Accueil.jsx
@@ -102,6 +102,7 @@ const Accueil = ({ players, onNav, user, ligue, onLigues, onRoadmap }) => {
               </div>
             ))}
           </div>
+          <div className="h-6" />
         </>
       )}
     </div>
